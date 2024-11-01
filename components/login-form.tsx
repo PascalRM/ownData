804efd2164dd06a665e1eff071e53120ccd7f63a
pascal <pascal.ramanathan@gmail.com>
2024-11-01 16:32:37 +0100

    adds login fn and betterauth

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
@@ -10,8 +11,33 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { authClient } from "@/lib/auth-client"
+import { useState } from "react"
 
 export function LoginForm() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  async function login(e: React.FormEvent) {
+    e.preventDefault();
+    
+    // const { data, error } = await authClient.signUp.email({
+    //   email: "pascal.ramanathan@gmail.com",
+    //   password: "PascalRamanathan$123",
+    //   name: "Pascal",
+    // });
+
+    setError("");
+    const { data, error } = await authClient.signIn.email({
+      email: email ? email : "",
+      password: password ? password : "",
+      callbackURL: "/"
+    });
+
+    if(error && error.message) setError(error.message);
+  }
+
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
@@ -21,32 +47,37 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link>
+        <form onSubmit={login}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <Input id="password" type="password" required />
-          </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-          {/* <Button variant="outline" className="w-full">
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link href="#" className="ml-auto inline-block text-sm underline">
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required
+                onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            {error && <p className="text-sm">{error}</p>}
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+            {/* <Button variant="outline" className="w-full">
             Login with Google
-          </Button> */}
-        </div>
+            </Button> */}
+          </div>
+        </form>
         {/* <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
           <Link href="#" className="underline">
---
+++ lib/auth.ts
@@ -0,0 +1,19 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db"; // your drizzle instance
+import { account, session, user, verification } from "@/db/schema";
+
+export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: "pg", // or "mysql", "sqlite",
+        schema: {
+            user,
+            session,
+            account,
+            verification
+        },
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
+});
